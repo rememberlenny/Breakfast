13e513118db1ae7f0fc23119385ca84fed2ffbe8
rememberlenny <rememberlenny@gmail.com>
2014-12-25 23:25:59 -0500

    Save the proper routed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_filter :ensure_signup_complete, only: [:new, :create, :update, :destroy]
-  before_filter :configure_permitted_parameters
+  before_filter :configure_permitted_parameters, only: [:update]
 
   protected
 
   # my custom fields are :name, :heard_how
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:name,
+      u.permit(:name, :password, :password_confirmation, :current_password,
       :email, :tod_morning_early, :tod_morning_middle, :tod_morning_late, :dow_monday, :dow_tuesday, :dow_wednesday, :dow_thursday, :dow_friday, :dow_saturday, :dow_sunday )
     end
   end
