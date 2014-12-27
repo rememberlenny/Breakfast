92c4af286fd3a00fe59ef97703157c85e4def516
rememberlenny <rememberlenny@gmail.com>
2014-12-26 19:19:53 -0500

    Save the user times

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,6 +1,21 @@
 class MeetingsController < ApplicationController
   def index
     @users = User.all
+    early  = []
+    middle = []
+    late   = []
+
+    @users.each do |user|
+      if user.tod_morning_early == 1
+        @early << user
+      end
+      if user.tod_morning_middle == 1
+        @middle << user
+      end
+      if user.tod_morning_late == 1
+        @late << user
+      end
+    end
   end
 
   def show
